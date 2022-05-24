981927efd489b26eb26727e15567448c3b74c49a
steve3j <sgioja@gmail.com>
2022-05-23 23:00:45 -0700

    heroku and docker added

--- api/knexfile.js
+++ api/knexfile.js
@@ -3,45 +3,45 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+ const HOST = process.env.DATABASE_HOST || '127.0.0.1';
+ const USER = process.env.POSTGRES_USER || 'postgres';
+ const PASSWORD = process.env.POSTGRES_PASSWORD || 'docker';
+ const DATABASE = process.env.POSTGRES_DB || 'blogapp_db';
+ const PORT = process.env.PORT || 5432;
+
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: "postgresql",
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: HOST,
+      password: PASSWORD,
+      user: USER,
+      port: PORT,
+      database: DATABASE,
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL+'?ssl=no-verify',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
-  }
+  },
 
 };
