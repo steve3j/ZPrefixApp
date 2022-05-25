86b73e31e0703e54fb9ea1056d76ac1515c01a91
steve3j <sgioja@gmail.com>
2022-05-24 18:48:09 -0700

    add migrations

--- api/migrations/20220524060425_Users.js
+++ api/migrations/20220524060425_Users.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable("Users", (table) => {
+        table.increments();
+        table.string("first_name", 50)
+        table.string("last_name", 50)
+        table.string("username", 50)
+        table.string("password", 50)
+    })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('Users');
 };
--- api/migrations/20220524060431_Posts.js
+++ api/migrations/20220524060431_Posts.js
@@ -2,14 +2,27 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable("Posts", (table) => {
+        table.increments();
+        table.string("user_id", 50)
+        table.foreign("user_id").references("users.id")
+        table.string("title", 50)
+        table.string("content", 1000)
+        table.string("password", 50)
+    })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema
+        .alterTable("users", (table) => {
+            table.dropForeign("users_id")
+        })
+        .then(function () {
+            return knex.schema.dropTableIfExists('Posts');
+        })
 };
