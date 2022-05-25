5af61ef070050c17596d6a3382bbb965913fc4cf
steve3j <sgioja@gmail.com>
2022-05-24 18:08:31 -0700

    api port set as env or 3001

--- api/app.js
+++ api/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors');
 const bcrypt = require ('bcrypt')
 const saltRounds = 10
 var password = "Fkdj^45ci@Jad"
 var hashed = ''
 
+app.use(cors());
+const env = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[env]
+const knex = require('knex')(config)
 
 bcrypt.hash(password, saltRounds, function(err, hash) {
     hashed = hash
@@ -16,6 +20,16 @@ bcrypt.compare(password, hashed, function(err, result) {
     console.log('compareResults: ', result)
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.status(200).send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+// app.get('/authors', (request, response) => {
+//     knex('app_authors')
+//         .select('*')
+//         .then(authorRecords => {
+//             let responseData = authorRecords.map(author => ({ firstName: author.first_name, lastName: author.last_name}));
+//             response.status(200).send(responseData)
+//         })
+
+// })
+
+module.exports = app;
